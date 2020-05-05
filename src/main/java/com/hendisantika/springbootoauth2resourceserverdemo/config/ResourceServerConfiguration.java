2ed3500a04fd78a8d107e121bfb0bef027486a09
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-06 06:34:07 +0700

    #9 - Add more beans in ResourceServerConfiguration

--- src/main/java/com/hendisantika/springbootoauth2resourceserverdemo/config/ResourceServerConfiguration.java
+++ src/main/java/com/hendisantika/springbootoauth2resourceserverdemo/config/ResourceServerConfiguration.java
@@ -1,8 +1,14 @@
 package com.hendisantika.springbootoauth2resourceserverdemo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,5 +22,28 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId("resource-server-rest-api").authenticationManager(authenticationManagerBean())
+                .tokenExtractor(new CustomTokenExtractor());
+    }
 
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        OAuth2AuthenticationManager authenticationManager = new OAuth2AuthenticationManager();
+        authenticationManager.setTokenServices(tokenService());
+        return authenticationManager;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable().anonymous().and().authorizeRequests().antMatchers("/user/**").authenticated()
+                .antMatchers("/public/**").permitAll();
+    }
+
+    @Bean
+    public ResourceServerTokenServices tokenService() {
+        CustomRemoteTokenService tokenServices = new CustomRemoteTokenService();
+        return tokenServices;
+    }
 }
