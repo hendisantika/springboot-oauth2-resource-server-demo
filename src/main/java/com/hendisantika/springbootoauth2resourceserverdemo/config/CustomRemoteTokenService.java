e886fdecb55b4a9a0a4c7474ec5e2c659aed4e04
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-06 06:31:45 +0700

    #5 - Add loadAuthentication, readAccessToken & executeGet method in CustomRemoteTokenService

--- src/main/java/com/hendisantika/springbootoauth2resourceserverdemo/config/CustomRemoteTokenService.java
+++ src/main/java/com/hendisantika/springbootoauth2resourceserverdemo/config/CustomRemoteTokenService.java
@@ -1,12 +1,26 @@
 package com.hendisantika.springbootoauth2resourceserverdemo.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,6 +31,7 @@ import java.io.IOException;
  * Date: 06/05/20
  * Time: 06.26
  */
+@Slf4j
 public class CustomRemoteTokenService implements ResourceServerTokenServices {
 
     private final RestOperations restTemplate;
@@ -35,5 +50,38 @@ public class CustomRemoteTokenService implements ResourceServerTokenServices {
                 }
             }
         });
+    }
+
+    @Override
+    public OAuth2Authentication loadAuthentication(String accessToken) throws AuthenticationException,
+            InvalidTokenException {
+        HttpHeaders headers = new HttpHeaders();
+        Map<String, Object> map = executeGet("http://localhost:8095/oauth/check_token?token=" + accessToken, headers);
+        if (map == null || map.isEmpty() || map.get("error") != null) {
+            throw new InvalidTokenException("Token not allowed");
+        }
+        return tokenConverter.extractAuthentication(map);
+    }
+
+    @Override
+    public OAuth2AccessToken readAccessToken(String accessToken) {
+        throw new UnsupportedOperationException("Not supported: read access token");
+    }
+
+    private Map<String, Object> executeGet(String path, HttpHeaders headers) {
+        try {
+            if (headers.getContentType() == null) {
+                headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+            }
+            @SuppressWarnings("rawtypes")
+            Map map = restTemplate.exchange(path, HttpMethod.GET, new HttpEntity<MultiValueMap<String, String>>(null,
+                    headers), Map.class).getBody();
+            @SuppressWarnings("unchecked")
+            Map<String, Object> result = map;
+            return result;
+        } catch (Exception ex) {
+            log.info(ex.getMessage());
+        }
+        return null;
     }
 }
